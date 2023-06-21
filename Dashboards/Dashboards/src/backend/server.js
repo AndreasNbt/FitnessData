5402e158e0748a1dbf4d0874613eba402e072532
Andreas <andreasn@csd.auth.gr>
2023-06-21 18:34:58 +0300

    add comments to server.js file

--- Dashboards/Dashboards/src/backend/server.js
+++ Dashboards/Dashboards/src/backend/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const apiRoutes = require('./api');
 
+// Create an instance of Express
 const app = express();
 
+// Set up middleware
 app.use(express.json());
 app.use(cors());
 
+// Set up API routes
 app.use('/api', apiRoutes);
 
+// Set up server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
